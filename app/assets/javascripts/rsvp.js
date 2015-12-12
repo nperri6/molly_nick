8dce310c4c71fc400d7af77b8669f905fd770b13
Nick Perri <nperri6@gmail.com>
2015-12-12 17:28:55 -0500

    Add flipping effect to rsvp  card

--- app/assets/javascripts/rsvp.js
+++ app/assets/javascripts/rsvp.js
@@ -12,4 +12,29 @@ $(document).ready(function(){
       $('.additional-guests').append(data);
     })
   });
+
+  // var cards = document.querySelectorAll(".card.effect__click");
+  // for ( var i  = 0, len = cards.length; i < len; i++ ) {
+  //   var card = cards[i];
+  //   clickListener( card );
+  // }
+
+  $('#rsvpButton').on('click', function(action){
+    action.preventDefault();
+    $('.card__front').addClass('flipped');
+    $('.card__back').addClass('flipped-back');
+    curdata = $(this).parent().parent().serialize()
+    $.ajax({
+      method: 'post',
+      url: '/guests/create',
+      data: curdata
+    })
+    .done(function(data){})
+  })
+  function clickListener(card) {
+    card.addEventListener( "click", function() {
+      var c = this.classList;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    });
+  }
 });
