0eed3fa5a5c9173b4a0add039ea58b0c634309f3
Nick Perri <nperri6@gmail.com>
2015-12-12 17:31:18 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,8 @@ ActiveRecord::Schema.define(version: 20151206031919) do
   enable_extension "plpgsql"
 
   create_table "guests", force: :cascade do |t|
-    t.string   "name",       null: false
+    t.string   "firstname",  null: false
+    t.string   "lastname",   null: false
     t.string   "attending",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
