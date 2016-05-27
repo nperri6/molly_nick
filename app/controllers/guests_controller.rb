e785abbb2c8ce95c0b9858951c22b1d5caa5c018
Nick Perri <nperri6@gmail.com>
2016-05-27 09:46:34 -0400

    Fix rsvp submit issues

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,10 +1,13 @@
 class GuestsController < ApplicationController
   def create
     params["firstnames"].each_with_index do |name, index|
+      puts("name",name)
+      puts(params)
       new_guest = Guest.new(firstname: params["firstnames"][index], lastname: params["lastnames"][index], attending: params["attending"][index.to_s])
+      puts(new_guest)
       new_guest.save
-      render text: ""
     end
+    render text: ""
   end
 
   def new
